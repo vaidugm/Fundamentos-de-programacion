55682594b6182263123877bd8c08058b956daba5
Vaidu Muñoz <115045666+vaidugm@users.noreply.github.com>
2025-08-28 04:10:53 +0000

    terminado

--- semestre1/tarea1.py
+++ semestre1/tarea1.py
@@ -1,5 +1,5 @@
-print("~~~~Que deseas realizar~~~~")
-print("1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir")
+print( "~~~~Que deseas realizar~~~~" )
+print( "1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir" )
 
 problema = int(input("Elije la opcion a realizar: "))
 
@@ -7,10 +7,10 @@ num1 = int(input ( "Ingresa un numero: " ) )
 num2 = int(input ( "Ingresa un numero: " ) )
 
 if problema == 1:
-    print ( "La solucion a tu problema", num1 + num2 )    
+    print ( "La solucion a tu suma es: ", num1 + num2 )    
 elif problema == 2:
-    print ( "La solucion a tu problema", num1 - num2 )
+    print ( "La solucion a tu resta es: ", num1 - num2 )
 elif problema == 3:
-    print ( "La solucion a tu problema", num1 * num2 )
+    print ( "La solucion a tu multiplicacion es: ", num1 * num2 )
 elif problema == 4:
-    print ( "La solucion a tu problema", num1 / num2 )
+    print ( "La solucion a tu division es: ", num1 / num2 )
