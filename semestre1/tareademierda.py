0e4522b22da7ab6501a551d3d950d0ef2a5aba72
Vaidu Muñoz <115045666+vaidugm@users.noreply.github.com>
2025-08-28 04:23:40 +0000

    xd

---
+++ semestre1/tareademierda.py
@@ -0,0 +1,23 @@
+#Suma
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa un numero: "))
+
+print ( "Resultado: ", num1 + num2 )
+
+#Resta
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa un numero: "))
+
+print ( "Resultado: ", num1 - num2 )
+
+#Multiplicacion
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa un numero: "))
+
+print ( "Resultado: ", num1 * num2 )
+
+#Divicion
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa un numero: "))
+
+print ( "Resultado: ", num1 / num2 )
