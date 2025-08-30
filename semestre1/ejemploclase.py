37496ac3cb0d442d7cf526bb793301f5d38e206b
Vaidu Muñoz <115045666+vaidugm@users.noreply.github.com>
2025-08-30 17:54:15 +0000

    update example class

--- semestre1/ejemploclase.py
+++ semestre1/ejemploclase.py
@@ -1,7 +1,7 @@
-print ("~~~~Ingresa tus datos personales~~~~")
-nombre = input("Ingresa tu nombre: ")
-apm = input("Ingresa tus apellidos: ")
-numcontrol = input("Ingresa tu numero de control: ")
-carrera = input("Ingresa tu carrera: ")
+print ( "~~~~Ingresa tus datos personales~~~~" )
+nombre = input( "Ingresa tu nombre: " )
+apm = input( "Ingresa tus apellidos: " )
+numcontrol = input( "Ingresa tu numero de control: " )
+carrera = input( "Ingresa tu carrera: " )
 
-print (f"Tu nombre completo es {nombre} {apm}\nTu numero de control es {numcontrol}\nTu carrera es {carrera}")
+print ( f"Tu nombre completo es {nombre} {apm}\nTu numero de control es {numcontrol}\nTu carrera es {carrera}" )
