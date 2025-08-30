8a871526251221c5bca051f01792f4147ea78f3f
Vaidu Muñoz <115045666+vaidugm@users.noreply.github.com>
2025-08-30 16:25:21 +0000

    update

---
+++ semestre1/tarea_1_sencilla.py
@@ -0,0 +1,19 @@
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa un numero: "))
+
+print ( "Resultado: ", num1 + num2 )
+
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa un numero: "))
+
+print ( "Resultado: ", num1 - num2 )
+
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa un numero: "))
+
+print ( "Resultado: ", num1 * num2 )
+
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa un numero: "))
+
+print ( "Resultado: ", num1 / num2 )
