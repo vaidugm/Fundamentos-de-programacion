350f7153c60d74df8e3071465617868023032cf8
Vaidu Muñoz <115045666+vaidugm@users.noreply.github.com>
2025-10-04 15:53:09 +0000

    a

--- a.py
+++ a.py
@@ -1,23 +1,26 @@
-import sys
-from rich import print
-from time import sleep
 import time
 
 def printlyrics():
-    line = {
+    lines = [
         ("I wanna da-", 0.06),
-        ("I wanna dance in the ligths", 0.05),
+        ("I wanna dance in the lights", 0.05),
         ("I wanna ro-", 0.07),
         ("I wanna rock your body", 0.08),
         ("I wanna go-", 0.08),
         ("I wanna go for a ride", 0.068),
         ("Hop in the music and", 0.07),
-        ("Rock you body", 0.08),
-        ("Rock you body", 0.069),
+        ("Rock your body", 0.08),
+        ("Rock your body", 0.069),
         ("come on, come on", 0.035),
-        ("Rock you body", 0.05),
-        ("(Rock you body)", 0.03),
-        ("Rock you body", 0.049),
+        ("Rock your body", 0.05),
+        ("(Rock your body)", 0.03),
+        ("Rock your body", 0.049),
         ("come on, come on", 0.035),
-        ("Rock you body", 0.08),
-    }
+        ("Rock your body", 0.08),
+    ]
+
+    for text, delay in lines:
+        print(text, flush=True)
+        time.sleep(delay)
+
+printlyrics()
