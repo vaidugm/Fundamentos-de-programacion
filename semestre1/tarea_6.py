160312c1b6934c88452371b3913774216ff3a03d
Vaidu Muñoz <115045666+vaidugm@users.noreply.github.com>
2025-10-28 00:36:42 +0000

    fix

--- semestre1/tarea_6.py
+++ semestre1/tarea_6.py
@@ -9,13 +9,18 @@ def menu():
 
         if n == "1":
             lado = float(input("Ingresa la medida del lado del cuadrado: "))
+
             print (f"El perimetro del cuadrado es: {4 * lado}\n")
+
         elif n == "2":
             largo = float(input("Ingresa la medida del largo del rectangulo: "))
             ancho = float(input("Ingresa la medida del ancho del rectangulo: "))
+
             print (f"El perimetro del rectangulo es: {2 * (largo + ancho)}\n")
+
         elif n == "3":
             break
+        
         else:
             print("Opcion no valida. Por favor, intenta de nuevo.\n")
 menu()
