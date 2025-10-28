5716f0df53addde812c18d26c10ba82b51817eaf
Vaidu Muñoz <115045666+vaidugm@users.noreply.github.com>
2025-10-28 00:34:18 +0000

    a

--- tarea_6.py
+++ tarea_6.py
@@ -5,15 +5,15 @@ def menu():
         print ("Perimetro del Rectangulo (2)")
         print ("Cerrar programa (3)")
 
-        n = input("Ingresa el numero de la opcion que deseas: ")   
+        n = input ("Ingresa el numero de la opcion que deseas: ")   
 
         if n == "1":
             lado = float(input("Ingresa la medida del lado del cuadrado: "))
-            print(f"El perimetro del cuadrado es: {4 * lado}\n")
+            print (f"El perimetro del cuadrado es: {4 * lado}\n")
         elif n == "2":
             largo = float(input("Ingresa la medida del largo del rectangulo: "))
             ancho = float(input("Ingresa la medida del ancho del rectangulo: "))
-            print(f"El perimetro del rectangulo es: {2 * (largo + ancho)}\n")
+            print (f"El perimetro del rectangulo es: {2 * (largo + ancho)}\n")
         elif n == "3":
             break
         else:
